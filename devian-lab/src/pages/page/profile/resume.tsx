d993a8f76662cc234277c4b1097c18f9ff493f4c
j.ian <jay2u8809@gmail.com>
2024-09-01 18:49:42 +0900

    change profile page

---
+++ devian-lab/src/pages/page/profile/resume.tsx
@@ -0,0 +1,11 @@
+import Layout from '@theme/Layout';
+
+export default function Index(): JSX.Element {
+  return (
+    <Layout
+      title={`resume page`}
+      description="resume">
+      RESUME
+    </Layout>
+  );
+}
